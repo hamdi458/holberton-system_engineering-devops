4b19bb284aabd6b32aa6eb0dbd63b81938576e89
hamdi458 <hamdi.645@gmail.com>
2020-09-14 16:31:16 +0100

    req

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
         format(id)).json()
     f = open('{}.csv'.format(id), 'w')
     with f:
-        row = csv.writer(f, delimiter=',', quoting=csv.QUOTE_ALL)
+        line = csv.writer(f, delimiter=',', quoting=csv.QUOTE_ALL)
         for obj in response_tasks_t_f:
-            row.writerow([id, response.get('username'),
-                          obj.get('completed'), obj.get('title')])
+            line.writerow([id, response.get('username'),
+                          obj['completed'], obj.get('title')])
