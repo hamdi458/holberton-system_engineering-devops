92798ddd9aa56c2e8eb16cbba006800bfc53a7f7
hamdi458 <hamdi.645@gmail.com>
2020-09-17 21:33:59 +0100

    wiw

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+API
+"""
+
+import requests
+
+def recurse(subreddit, hot_list=[], after=""):
+    url = 'http://reddit.com/r/{}/hot.json'.format(subreddit)
+    header = {'User-agent': 'hama_chrab'}
+    res = requests.get(url, headers=header,
+                       params={'t': all, 'after': after})
+    if not res or res.status_code == 400:
+        return None
+    for i in res.json().get('data')['children']:
+        hot_list.append(i['data']['title'])
+    after = res.json().get('data').get('after')
+    if after:
+        recurse(subreddit, hot_list, after)
+    return hot_list
