8c7917b87b3d85e47ff1cc745a8e4a03fdbe5c84
hamdi458 <hamdi.645@gmail.com>
2020-09-17 21:02:14 +0100

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,14 @@ import requests
 
 def number_of_subscribers(subreddit):
 
-    header = {'User-Agent': 'Mozilla/5.0'}
+    h = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0)"
+    e = "Gecko/20100101 Firefox/80.0"
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    res = requests.get(url, headers=header, allow_redirects=False).json()[
-        "data"]["subscribers"]
+    res = requests.get(
+        url,
+        headers={
+            'User-Agent': h + e},
+        allow_redirects=False).json()["data"]["subscribers"]
     try:
         return (res)
     except BaseException:
